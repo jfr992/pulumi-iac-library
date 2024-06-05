80a9b7a4ff05190237f42abf27fba3886a364caa
juan.reyes <juan.reyes@yum.com>
2024-06-04 21:09:01 -0500

    testing github actions

--- main.go
+++ main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/s3"
+	alb "github.com/jfr992/pulumi-packages/alb"
+	asg "github.com/jfr992/pulumi-packages/asg"
+	network "github.com/jfr992/pulumi-packages/network"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create an AWS resource (S3 Bucket)
-		bucket, err := s3.NewBucket(ctx, "my-bucket", nil)
+		configFile := "infra.yaml"
+
+		userdata := "userdata.sh"
+
+		vpcID, privateSubnetIds, publicSubnetIds, err := network.CreateNetwork(ctx, configFile)
 		if err != nil {
 			return err
 		}
 
-		// Export the name of the bucket
-		ctx.Export("bucketName", bucket.ID())
+		targetGroupArn, securityGroupID, err := alb.CreateALB(ctx, configFile, vpcID, privateSubnetIds)
+		if err != nil {
+			return err
+		}
+
+		err = asg.CreateASG(ctx, configFile, userdata, vpcID, privateSubnetIds, targetGroupArn, securityGroupID)
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("vpcID", vpcID)
+		ctx.Export("privateSubnetIds", privateSubnetIds)
+		ctx.Export("publicSubnetIds", publicSubnetIds)
+		ctx.Export("targetGroupArn", targetGroupArn)
+		ctx.Export("securityGroupID", securityGroupID)
+
 		return nil
 	})
 }
